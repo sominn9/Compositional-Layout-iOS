4489da6a4a15e8fd7c9723b4a2c14b61ee847a53
somin3415 <somin3415@gmail.com>
2021-09-28 19:52:16 +0900

    Initial Commit

---
+++ Compositional-Layout-Examples/Compositional_Layout_ExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Compositional_Layout_ExamplesApp.swift
+//  Compositional-Layout-Examples
+//
+//  Created by 신소민 on 2021/09/28.
+//
+
+import SwiftUI
+
+@main
+struct Compositional_Layout_ExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Compositional-Layout-Examples/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Compositional-Layout-Examples
+//
+//  Created by 신소민 on 2021/09/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
